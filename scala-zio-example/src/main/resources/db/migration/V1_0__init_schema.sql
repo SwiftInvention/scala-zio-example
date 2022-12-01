a2ca135044ce8a255fc2832fb15de8fd047749eb
Vladimir Logachev <vladimir@logachev.dev>
2022-12-01 18:24:20 +0400

    Store timestamps as timestamps

--- scala-zio-example/src/main/resources/db/migration/V1_0__init_schema.sql
+++ scala-zio-example/src/main/resources/db/migration/V1_0__init_schema.sql
@@ -1,13 +1,11 @@
-CREATE TABLE IF NOT EXISTS person
-(
-    name       varchar(255),
-    birth_date DATETIME NOT NULL,
+CREATE TABLE IF NOT EXISTS person (
+    name varchar(255) NOT NULL,
+    birth_date timestamp NOT NULL,
     PRIMARY KEY (name)
 );
 
-INSERT IGNORE INTO person(name, birth_date)
-VALUES ('Martin Odersky', '1958-09-05 00:42:01');
-INSERT IGNORE INTO person(name, birth_date)
-VALUES ('James Gosling', '1955-05-19 01:42:01');
-INSERT IGNORE INTO person(name, birth_date)
-VALUES ('Mister X', now());
+INSERT INTO person (name, birth_date)
+    VALUES ('Justin Bieber', FROM_UNIXTIME (762469200));
+
+INSERT INTO person (name, birth_date)
+    VALUES ('Elon Musk', FROM_UNIXTIME (46900800));
