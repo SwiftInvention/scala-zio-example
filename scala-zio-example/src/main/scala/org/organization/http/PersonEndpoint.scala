e61548c216761e345a10fa8037153420743e6fca
VKFisher <vladkf@gmail.com>
2022-12-20 19:00:15 +0400

    add "get person by id" endpoint

--- scala-zio-example/src/main/scala/org/organization/http/PersonEndpoint.scala
+++ scala-zio-example/src/main/scala/org/organization/http/PersonEndpoint.scala
@@ -1,25 +1,47 @@
 package org.organization.http
 
+import io.circe.generic.auto._
+import org.organization.AppEnv.AppEnv
 import org.organization.api.to.PersonTO
 import org.organization.db.repository.PersonRepository
-import org.organization.AppEnv.AppEnv
 import sttp.tapir._
-import io.circe.generic.auto._
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe._
 import sttp.tapir.ztapir.ZTapir
+import zio.ZIO
 
 object PersonEndpoint extends PersonRepository with ZTapir {
 
-  val personListing: PublicEndpoint[Unit, Unit, List[PersonTO], Any] =
+  private val personListing: PublicEndpoint[Unit, Unit, List[PersonTO], Any] =
     endpoint.get
       .name("Default-endpoint")
       .description("Get all persons from database")
       .in("person" / "list" / "all")
       .out(jsonBody[List[PersonTO]])
 
+
   // TODO: Provide error body, not just Unit
   val personListingServerLogic: ZServerEndpoint[AppEnv, Any] =
     personListing.zServerLogic(_ => getAllPersons.map(_.map(_.toTO())).mapError(_ => ()))
+
+  private val personById: PublicEndpoint[Long, Unit, PersonTO, Any] =
+    endpoint.get
+      .name("get-person")
+      .description("Get person from database by id")
+      .in("person" / path[Long]("personId"))
+      .out(jsonBody[PersonTO])
+
+  // TODO: Provide error body, not just Unit
+  val personByIdServerLogic: ZServerEndpoint[AppEnv, Any] = {
+    personById.zServerLogic(
+      personId => getById(personId)
+        .mapError(_ => ())
+        .flatMap({
+          case None => ZIO.fail(())
+          case Some(person) => ZIO.succeed(person.toTO())
+        })
+    )
+  }
+
 
 }
--- scala-zio-example/src/main/scala/org/organization/http/swagger/SwaggerApiEndpoint.scala
+++ scala-zio-example/src/main/scala/org/organization/http/swagger/SwaggerApiEndpoint.scala
@@ -1,21 +1,22 @@
 package org.organization.http.swagger
 
-import org.organization.http.PersonEndpoint.personListingServerLogic
-import org.organization.http.check.HealthCheckEndpoint
 import org.organization.AppEnv.AppEnv
+import org.organization.http.PersonEndpoint.{personByIdServerLogic, personListingServerLogic}
+import org.organization.http.check.HealthCheckEndpoint
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import sttp.tapir.ztapir.ZTapir
 
 object SwaggerApiEndpoint extends ZTapir {
-  val apiEndpoints: List[ZServerEndpoint[AppEnv, Any]] =
+  private val apiEndpoints: List[ZServerEndpoint[AppEnv, Any]] =
     List(
       HealthCheckEndpoint.healthCheckingServerEndpoint,
       HealthCheckEndpoint.readinessCheckingServerEndpoint,
-      personListingServerLogic
+      personListingServerLogic,
+      personByIdServerLogic
     )
 
-  val docEndpoints: List[ZServerEndpoint[AppEnv, Any]] = SwaggerInterpreter()
+  private val docEndpoints: List[ZServerEndpoint[AppEnv, Any]] = SwaggerInterpreter()
     .fromServerEndpoints(apiEndpoints, "project", "0.1.0")
 
-  val common = apiEndpoints ++ docEndpoints
+  val common: List[SwaggerApiEndpoint.ZServerEndpoint[AppEnv, Any]] = apiEndpoints ++ docEndpoints
 }
