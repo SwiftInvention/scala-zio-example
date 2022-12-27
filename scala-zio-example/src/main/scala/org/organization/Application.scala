87e4da3e03b6efdd27f88781f8c27d3e6a527bd2
Vladimir Logachev <vladimir@logachev.dev>
2022-12-27 18:04:34 +0400

    Implement intentional leak

--- scala-zio-example/src/main/scala/org/organization/Application.scala
+++ scala-zio-example/src/main/scala/org/organization/Application.scala
@@ -12,8 +12,6 @@ import zio._
 import java.time.Duration
 
 object Application extends App {
-  // FIXME: for an unknown reason, the specified duration of 10 seconds simply doesn't work,
-  // And the mysql connection timeout is used instead.
   private val composedMiddlewares = Middleware.timeout(Duration.ofSeconds(10))
 
   private val app: HttpApp[AppEnv, Throwable] =
