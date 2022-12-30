41cdd57a3e4d1b5bc9b36a429ccd0aedec9d7df9
VKFisher <vladkf@gmail.com>
2022-12-30 16:39:48 +0400

    safe comparison op

--- scala-zio-example/src/test/scala/org/organization/ApplicationSpec.scala
+++ scala-zio-example/src/test/scala/org/organization/ApplicationSpec.scala
@@ -1,12 +1,13 @@
 package org.organization
 
+import cats.implicits.catsSyntaxEq
 import zio.test._
 
 object ApplicationSpec {
   def spec: ZSpec[Any, Nothing] =
     suite("Example unit test suite")(
       test("math works") {
-        assertTrue(1 + 1 == 2)
+        assertTrue(1 + 1 === 2)
       }
     )
 }
