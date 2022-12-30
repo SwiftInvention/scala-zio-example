ea094bcb3078b4a670d6df82980f2d48452ce4f6
VKFisher <vladkf@gmail.com>
2022-12-30 16:33:08 +0400

    add unit test example

--- scala-zio-example/src/test/scala/org/organization/ApplicationSpec.scala
+++ scala-zio-example/src/test/scala/org/organization/ApplicationSpec.scala
@@ -1,3 +1,12 @@
-object ApplicationSpec {
+package org.organization
 
+import zio.test._
+
+object ApplicationSpec {
+  def spec: ZSpec[Any, Nothing] =
+    suite("Example unit test suite")(
+      test("math works") {
+        assertTrue(1 + 1 == 2)
+      }
+    )
 }
