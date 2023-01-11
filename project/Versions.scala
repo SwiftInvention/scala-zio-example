2677318ff7649bf74f0e157b6d4a91e5505d1a3d
Scala Steward <scala_steward@virtuslab.com>
2023-01-11 19:02:03 +0000

    Update zio, zio-test, zio-test-sbt to 2.0.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val mysql      = "8.0.31"
   val quill      = "3.19.0" // Note: Upgrading to 4.x brings ZIO 2.x
   val tapir      = "1.2.3"
-  val zio        = "1.0.17" // Note: Upgrading to 2.x may break compatibility with internal libs
+  val zio        = "2.0.5"  // Note: Upgrading to 2.x may break compatibility with internal libs
   val pureConfig = "0.17.2"
   val zioHttp           = "1.0.0.0-RC29" // Note: Upgrading to 2.x brings ZIO 2.x
   val zioTestContainers = "0.9.0"
