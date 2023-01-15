9905488f9286ffee4bd7fd9c25c053f7e38df889
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-15 12:19:35 +0100

    Update tapir-core, tapir-json-circe, ... to 1.2.5 (#27)

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val flyway     = "9.10.2"
   val mysql      = "8.0.31"
   val quill      = "3.19.0" // Note: Upgrading to 4.x brings ZIO 2.x
-  val tapir      = "1.2.3"
+  val tapir      = "1.2.5"
   val zio        = "1.0.17" // Note: Upgrading to 2.x may break compatibility with internal libs
   val pureConfig = "0.17.2"
   val zioHttp           = "1.0.0.0-RC29" // Note: Upgrading to 2.x brings ZIO 2.x
