e5e85c0753b36ab5a68687821c1b521666fc2cf9
Scala Steward <scala_steward@virtuslab.com>
2023-01-23 21:30:08 +0000

    Reformat with scalafmt 3.7.0
    Executed command: scalafmt --non-interactive

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "io.scalaland"                %% "chimney"       % Versions.chimney,
     "io.estatico"                 %% "newtype"       % Versions.newType,
     "com.softwaremill.sttp.tapir" %% "tapir-newtype" % Versions.tapir
-  ) map (_                         % Compile)
+  ) map (_ % Compile)
 
   lazy val dbDep: Seq[ModuleID] = Seq(
     "io.getquill"  %% "quill-jdbc"           % Versions.quill,
@@ -18,7 +18,7 @@ object Dependencies {
     "mysql"         % "mysql-connector-java" % Versions.mysql,
     "org.flywaydb"  % "flyway-core"          % Versions.flyway,
     "org.flywaydb"  % "flyway-mysql"         % Versions.flyway
-  ) map (_          % Compile)
+  ) map (_ % Compile)
 
   lazy val httpDep: Seq[ModuleID] = Seq(
     "com.softwaremill.sttp.tapir" %% "tapir-core"              % Versions.tapir,
@@ -27,12 +27,12 @@ object Dependencies {
     "com.softwaremill.sttp.tapir" %% "tapir-zio1-http-server"  % Versions.tapir,
     "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % Versions.tapir,
     "io.d11"                       % "zhttp_2.13"              % Versions.zioHttp
-  ) map (_                         % Compile)
+  ) map (_ % Compile)
 
   lazy val testDep: Seq[ModuleID] = Seq(
     "dev.zio"               %% "zio-test"                 % Versions.zio,
     "dev.zio"               %% "zio-test-sbt"             % Versions.zio,
     "io.github.scottweaver" %% "zio-testcontainers-mysql" % Versions.zioTestContainers,
     "io.github.scottweaver" %% "zio-db-migration-aspect"  % Versions.zioTestContainers
-  ) map (_                   % Test)
+  ) map (_ % Test)
 }
