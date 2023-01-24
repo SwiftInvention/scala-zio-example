026a404a03d2eaddb9c6ffd537a52db288f8ee8e
VKFisher <vladkf@gmail.com>
2023-01-24 14:06:40 +0400

    adjust entrypoint: show error on fail

--- scala-zio-example/src/main/scala/org/organization/Application.scala
+++ scala-zio-example/src/main/scala/org/organization/Application.scala
@@ -5,12 +5,22 @@ import org.organization.http.AppServer
 import org.organization.utils.db.Migration
 import zio.{ZIOAppDefault, _}
 
+import java.io.IOException
+
 object Application extends ZIOAppDefault {
   private val main: ZIO[AppEnv, Throwable, Nothing] = (
     Console.printLine("Initializing application")
       *> Migration.migrate
       *> AppServer.serve
   )
-  def run: UIO[ExitCode] =
-    main.provideLayer(AppEnv.buildLiveEnv).exitCode
+
+  def run: IO[IOException, ExitCode] = for {
+    executionResult <- main.provideLayer(AppEnv.buildLiveEnv).exit
+    exitCode <- {
+      executionResult match {
+        case Exit.Success(_)     => ZIO.succeed(ExitCode.success)
+        case Exit.Failure(error) => Console.printLine(error) *> ZIO.succeed(ExitCode.failure)
+      }
+    }
+  } yield exitCode
 }
