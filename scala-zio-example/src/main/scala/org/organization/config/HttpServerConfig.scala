bd73d51a84e49d0b00fadd863695f4abfe66093e
VKFisher <vladkf@gmail.com>
2023-01-24 13:53:39 +0400

    fix HttpServerConfig

--- scala-zio-example/src/main/scala/org/organization/config/HttpServerConfig.scala
+++ scala-zio-example/src/main/scala/org/organization/config/HttpServerConfig.scala
@@ -1,15 +1,17 @@
 package org.organization.config
 
 import pureconfig._
-import pureconfig.error.ConfigReaderFailures
 import pureconfig.generic.auto._
+import zio.http.ServerConfig
 import zio.{ZIO, ZLayer}
 
 final case class HttpServerConfig(host: String, port: Int)
 
 object HttpServerConfig {
-  val layer: ZLayer[Any, ConfigReaderFailures, HttpServerConfig] =
-    ZIO
-      .fromEither(ConfigSource.default.at("http-server").load[HttpServerConfig])
-      .toLayer
+  val layer: ZLayer[Any, Throwable, ServerConfig] =
+    ZLayer.fromZIO(for {
+      conf <- ZIO
+        .fromEither(ConfigSource.default.at("http-server").load[HttpServerConfig])
+        .mapError(err => new RuntimeException(s"could not get server config due to error: $err"))
+    } yield ServerConfig.default.binding(hostname = conf.host, port = conf.port))
 }
