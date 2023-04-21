d3e4d78af52d7645ab6d4db05facd60217f6354c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-04-21 11:52:08 +0200

    Update zio, zio-test, zio-test-sbt to 2.0.13 (#72)

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val mysql             = "8.0.32"
   val quill             = "4.6.0"
   val tapir             = "1.2.10"
-  val zio               = "2.0.10"
+  val zio               = "2.0.13"
   val pureConfig        = "0.17.2"
   val zioHttp           = "0.0.4"
   val zioTestContainers = "0.10.0"
