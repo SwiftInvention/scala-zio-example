a2f5f1c93ea122f8abd4e7e19a7e57fc9b5f58cd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-03 12:31:29 +0200

    Update tapir-core, tapir-json-circe, ... to 1.7.2 (#104)
    Co-authored-by: Vlad Fisher <51226323+VKFisher@users.noreply.github.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val flyway            = "9.21.0"
   val mysql             = "8.1.0"
   val quill             = "4.6.1"
-  val tapir             = "1.3.0"
+  val tapir             = "1.7.2"
   val zio               = "2.0.13"
   val pureConfig        = "0.17.6"
   val zioHttp           = "3.0.0-RC1"
