10d96528e299d39fb508cc7cab1bab9e8457ba63
VKFisher <vladkf@gmail.com>
2024-06-03 15:27:32 +0400

    bump zio-http + tapir, flyway, quill, zio-logging; fix breaks

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,17 +1,17 @@
 object Versions {
-  val mysql             = "8.4.0"
-  val flyway            = "9.21.2"
-  val quill             = "4.6.1"
-  val zio               = "2.0.22"
-  val tapir             = "1.7.2"
-  val pureConfig        = "0.17.6"
-  val zioHttp           = "3.0.0-RC1"
+  val mysql = "8.4.0"
+  val flyway = "10.12.0"
+  val quill = "4.8.3"
+  val zio = "2.0.22"
+  val tapir = "1.10.7"
+  val pureConfig = "0.17.6"
+  val zioHttp = "3.0.0-RC7"
   val zioTestContainers = "0.10.0"
-  val chimney           = "0.7.5"
+  val chimney = "0.7.5"
   val enumeratum =
     "1.7.0" // Note: Versions above 1.7.0 add Scala 3 support and are not binary compatible with previous versions
-  val newType    = "0.4.4"
-  val zioLogging = "2.1.17"
-  val slf4j      = "1.7.36" // We keep version 1.7.36 because it's used by quill
-  val circe      = "0.14.3"
+  val newType = "0.4.4"
+  val zioLogging = "2.3.0"
+  val slf4j = "1.7.36" // We keep version 1.7.36 because it's used by quill
+  val circe = "0.14.3"
 }
