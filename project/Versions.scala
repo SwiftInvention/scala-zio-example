f218f50ee13d45ced7b84f3a58f7de503ccb8a7c
Scala Steward <scala_steward@virtuslab.com>
2025-10-27 17:52:42 +0000

    Update mysql-connector-j to 9.5.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val mysql             = "8.4.0"
+  val mysql             = "9.5.0"
   val flyway            = "10.12.0"
   val quill             = "4.8.3"
   val zio               = "2.1.1"
