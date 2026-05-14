a13597b2da711ff69d7e95028f7809435cc53a83
VKFisher <vladkf@gmail.com>
2026-05-14 17:53:26 +0400

    fix format

--- modules/lib/common/src/main/scala/com/example/lib/common/domain/error/ErrorCategory.scala
+++ modules/lib/common/src/main/scala/com/example/lib/common/domain/error/ErrorCategory.scala
@@ -9,9 +9,9 @@ sealed trait ErrorCategory extends EnumEntry
 object ErrorCategory extends Enum[ErrorCategory] {
   val values: immutable.IndexedSeq[ErrorCategory] = findValues
 
-  case object Api          extends ErrorCategory
-  case object Backend      extends ErrorCategory
-  case object Domain       extends ErrorCategory
+  case object Api     extends ErrorCategory
+  case object Backend extends ErrorCategory
+  case object Domain  extends ErrorCategory
   // -- per-ctx --
   case object Customer     extends ErrorCategory
   case object Notification extends ErrorCategory
